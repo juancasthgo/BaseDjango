0635423cb84d0e241e16b83b3b45d89f331b3f33
Juan <castillohidalgojp@gmail.com>
2023-05-19 08:02:34 -0400

    video6

---
+++ app/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Marca, Producto
+
+# Register your models here.
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ["nombre","precio","nuevo","marca"]
+    list_editable = ["precio"]
+    search_fields = ["nombre","marca"]
+    list_filter = ["marca","nuevo"]
+    list_per_page =  10
+
+
+admin.site.register(Marca)
+admin.site.register(Producto, ProductoAdmin)
---
+++ app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=25)    
+    precio = models.IntegerField()
+    descripcion = models.TextField()
+    nuevo = models.BooleanField()
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
+    fecha_fabricacion = models.DateField()
+    imagen = models.ImageField(upload_to="productos", null=True)
+
+    def __str__(self):
+        return self.nombre
